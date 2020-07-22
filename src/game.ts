ee19ea77000a4be6c6304ed9f192c6ce16f3730e
Kyllian LE BORGNE ROEPRCH <me@hprivakos.net>
2020-07-22 04:42:49 +0200

    Video switch after timer

--- src/game.ts
+++ src/game.ts
@@ -1,64 +1,46 @@
-import utils from "../node_modules/decentraland-ecs-utils/index"
+import utils from '../node_modules/decentraland-ecs-utils/index'
+import { Delay } from '../node_modules/decentraland-ecs-utils/timer/component/delay'
 
+const videosList = [
+  { url: 'https://vod.dcl.guru/ko/190925/190925-.m3u8', duration: 5 },
+  { url: 'https://vod.dcl.guru/cryptochico/Litecoin-DeadOrDevelopedGithubExposedCanYouRelyonGithubData/Litecoin-DeadOrDevelopedGithubExposedCanYouRelyonGithubData.m3u8', duration: 1200 },
+  { url: 'https://vod.dcl.guru/cryptochico/MajorALTCOINBreakcoutNEXTWEEK-2CoinsSibosSpeculation/MajorALTCOINBreakcoutNEXTWEEK-2CoinsSibosSpeculation.m3u8', duration: 20 },
+  { url: 'https://vod.dcl.guru/cryptochico/RussianSpyConspiracyWillDestroyThese3CryptosDeepStateInvolved/RussianSpyConspiracyWillDestroyThese3CryptosDeepStateInvolved.m3u8', duration: 20 },
+  { url: 'https://vod.dcl.guru/cryptochico/SatoshiNakamotoRevealedOrJustAnotherFaketoshi/SatoshiNakamotoRevealedOrJustAnotherFaketoshi.m3u8', duration: 20 },
+  { url: 'https://vod.dcl.guru/cryptochico/TheNextAltcoinAltSeason-RealityorFairytaleMyth/TheNextAltcoinAltSeason-RealityorFairytaleMyth.m3u8', duration: 20 },
+]
+let videoRunning = -1
+let runningVideoTexture: VideoTexture
+let withinParcel: boolean
 
 // ------- building
 let cc = new Entity()
-cc.addComponent(new Transform({position: new Vector3(16,0,16)}))
-cc.addComponent(new GLTFShape("models/cc.glb"))
+cc.addComponent(new Transform({ position: new Vector3(16, 0, 16) }))
+cc.addComponent(new GLTFShape('models/cc.glb'))
 let animator = new Animator()
 cc.addComponent(animator)
-const clipText = new AnimationState("text") //spin
+const clipText = new AnimationState('text') //spin
 animator.addClip(clipText)
 clipText.play()
 engine.addEntity(cc)
 
-let coins = new Entity
-coins.addComponent(new Transform({position: new Vector3(16,0,16)}))
-coins.addComponent(new GLTFShape("models/coins.glb"))
+let coins = new Entity()
+coins.addComponent(new Transform({ position: new Vector3(16, 0, 16) }))
+coins.addComponent(new GLTFShape('models/coins.glb'))
 engine.addEntity(coins)
-
-
-// ----- video stream and trigger area
-// 1
-const myVideoClip = new VideoClip("https://vod.dcl.guru/cryptochico/Litecoin-DeadOrDevelopedGithubExposedCanYouRelyonGithubData/Litecoin-DeadOrDevelopedGithubExposedCanYouRelyonGithubData.m3u8")
-const myVideoTexture = new VideoTexture(myVideoClip)
-const myMaterial = new Material()
-myMaterial.specularIntensity = 0
-myMaterial.metallic = 0
-myMaterial.roughness = 1
-myMaterial.albedoTexture = myVideoTexture
-
-// 2 
-const myVideoClip2 = new VideoClip("https://vod.dcl.guru/cryptochico/MajorALTCOINBreakcoutNEXTWEEK-2CoinsSibosSpeculation/MajorALTCOINBreakcoutNEXTWEEK-2CoinsSibosSpeculation.m3u8")
-const myVideoTexture2 = new VideoTexture(myVideoClip2)
-const myMaterial2 = new Material()
-myMaterial2.specularIntensity = 0
-myMaterial2.metallic = 0
-myMaterial2.roughness = 1
-myMaterial2.albedoTexture = myVideoTexture2
 
 const screen = new Entity()
 screen.addComponent(new PlaneShape())
 screen.addComponent(
   new Transform({
     position: new Vector3(13.7, 4.5, 2.6),
-    scale: new Vector3(11.2, 6, 1)
+    scale: new Vector3(11.2, 6, 1),
   })
 )
-screen.addComponent(myMaterial)
 engine.addEntity(screen)
 
-
-
-
-// play the video texture
-myVideoTexture.playing = false
-myVideoTexture2.playing = false
 // create trigger area object, setting size and relative position
-let triggerBox = new utils.TriggerBoxShape(
-  new Vector3(32, 15, 30),
-  new Vector3(2, 0, 14)
-)
+let triggerBox = new utils.TriggerBoxShape(new Vector3(32, 15, 30), new Vector3(2, 0, 14))
 // Create trigger for entity
 screen.addComponent(
   new utils.TriggerComponent(
@@ -68,74 +50,111 @@ screen.addComponent(
     null, //onTriggerEnter
     null, //onTriggerExit
     () => {
+      if (runningVideoTexture == undefined) rollVideos(screen)
       //onCameraEnter
-      log("Enter")
-      myVideoTexture.playing = true
-      screen.addComponent(new utils.Delay(1000, () => {
-        myVideoTexture.playing = false
-        screen.addComponentOrReplace(myMaterial2)
-        myVideoTexture2.playing = true
-       }))
-
+      log('Enter')
+      withinParcel = true
+      runningVideoTexture.playing = true
+      /*       screen.addComponent(
+        new utils.Interval(5000, () => {
+          rollVideos(screen);
+        })
+      );
+ */
     },
     () => {
       //onCameraExit
-      log("Exit")
-      myVideoTexture.playing = false
-      myVideoTexture2.playing = false 
+      log('Exit')
+      screen.removeComponent(Delay)
+      withinParcel = false
+      runningVideoTexture.playing = false
     },
     false //debug
   )
 )
 
-
 // ------ social links
 let twetch = new Entity()
-twetch.addComponent(new Transform({position: new Vector3(16,0,16)}))
-twetch.addComponent(new GLTFShape("models/twetch.glb"))
+twetch.addComponent(new Transform({ position: new Vector3(16, 0, 16) }))
+twetch.addComponent(new GLTFShape('models/twetch.glb'))
 twetch.addComponent(
   new OnPointerDown(() => {
-    openExternalURL("https://twet.ch/inv/chicocrypto")
+    openExternalURL('https://twet.ch/inv/chicocrypto')
   })
-  )
-  engine.addEntity(twetch)
+)
+engine.addEntity(twetch)
 
 let instagram = new Entity()
-instagram.addComponent(new Transform({position: new Vector3(16,0,16)}))
-instagram.addComponent(new GLTFShape("models/instagram.glb"))
+instagram.addComponent(new Transform({ position: new Vector3(16, 0, 16) }))
+instagram.addComponent(new GLTFShape('models/instagram.glb'))
 instagram.addComponent(
   new OnPointerDown(() => {
-    openExternalURL("https://www.instagram.com/chicocrypto/?hl=en")
+    openExternalURL('https://www.instagram.com/chicocrypto/?hl=en')
   })
-  )
-  engine.addEntity(instagram)
+)
+engine.addEntity(instagram)
 
 let telegram = new Entity()
-telegram.addComponent(new Transform({position: new Vector3(16,0,16)}))
-telegram.addComponent(new GLTFShape("models/telegram.glb"))
+telegram.addComponent(new Transform({ position: new Vector3(16, 0, 16) }))
+telegram.addComponent(new GLTFShape('models/telegram.glb'))
 telegram.addComponent(
   new OnPointerDown(() => {
-    openExternalURL("https://t.me/joinchat/GfSdSEkwiQoQ1Q1...")
+    openExternalURL('https://t.me/joinchat/GfSdSEkwiQoQ1Q1...')
   })
-  )
-  engine.addEntity(telegram)
+)
+engine.addEntity(telegram)
 
 let uptrennd = new Entity()
-uptrennd.addComponent(new Transform({position: new Vector3(16,0,16)}))
-uptrennd.addComponent(new GLTFShape("models/uptrennd.glb"))
+uptrennd.addComponent(new Transform({ position: new Vector3(16, 0, 16) }))
+uptrennd.addComponent(new GLTFShape('models/uptrennd.glb'))
 uptrennd.addComponent(
   new OnPointerDown(() => {
-    openExternalURL("https://www.uptrennd.com/user/NzA3Mzk=")
+    openExternalURL('https://www.uptrennd.com/user/NzA3Mzk=')
   })
-  )
-  engine.addEntity(uptrennd)
+)
+engine.addEntity(uptrennd)
 
 let youtube = new Entity()
-youtube.addComponent(new Transform({position: new Vector3(16,0,16)}))
-youtube.addComponent(new GLTFShape("models/youtube.glb"))
+youtube.addComponent(new Transform({ position: new Vector3(16, 0, 16) }))
+youtube.addComponent(new GLTFShape('models/youtube.glb'))
 youtube.addComponent(
   new OnPointerDown(() => {
-    openExternalURL("https://www.youtube.com/channel/UCHop-jpf-huVT1IYw79ymPw")
+    openExternalURL('https://www.youtube.com/channel/UCHop-jpf-huVT1IYw79ymPw')
   })
-  )
-  engine.addEntity(youtube)
+)
+engine.addEntity(youtube)
+
+async function rollVideos(screen: Entity) {
+  videoRunning++
+  if (videoRunning >= videosList.length) videoRunning = 0
+  screen.removeComponent(Material)
+
+  const ourVideoClip = new VideoClip(videosList[videoRunning].url)
+  const ourVideoTexture = new VideoTexture(ourVideoClip)
+  const ourMaterial = new Material()
+  ourMaterial.specularIntensity = 0
+  ourMaterial.metallic = 0
+  ourMaterial.roughness = 1
+  ourMaterial.albedoTexture = ourVideoTexture
+  runningVideoTexture = ourVideoTexture
+
+  screen.addComponent(ourMaterial)
+  ourVideoTexture.playing = true
+  ourVideoTexture.loop = false
+}
+
+class VideoSystem {
+  timeSinceStart: number = 0
+  update(dt: number) {
+    let r = screen.getComponent(Material).albedoTexture as VideoTexture
+    if (r && withinParcel) {
+      this.timeSinceStart += dt
+      if (this.timeSinceStart >= videosList[videoRunning].duration) {
+        this.timeSinceStart = 0
+        rollVideos(screen)
+      }
+    }
+  }
+}
+
+engine.addSystem(new VideoSystem())
